3a605d4a7ef39b5cbefab64cea32beef660bd627
TharinduDeshananda <tdeshananda@gmail.com>
2024-08-28 20:05:34 +0530

    left navigator style changes

--- src/main/java/org/tdesh/panels/LeftNavigatorPanel.java
+++ src/main/java/org/tdesh/panels/LeftNavigatorPanel.java
@@ -10,7 +10,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class LeftNavigatorPanel extends JPanel {
-    public LeftNavigatorPanel(){
+    public LeftNavigatorPanel() {
         super();
         this.setBackground(MyColors.MAIN_BG_CL);
 
@@ -20,28 +20,38 @@ public class LeftNavigatorPanel extends JPanel {
 
         JLabel label = new JLabel(new ImageIcon(scaledImage));
         label.setBorder(new EmptyBorder(20, 30, 20, 30));
+
+        // Use GridLayout for equal width buttons
         Box box = new Box(BoxLayout.Y_AXIS);
-        this.add(box);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.NORTHWEST; // Align to the top left
+        gbc.weighty = 1.0; // Push the components to the top
+        this.add(box, gbc);
 
         box.add(label);
 
-        //add buttons
-        SideNavButton base64Btn = new SideNavButton("Base 64",()->{
+        // Add buttons
+        SideNavButton base64Btn = new SideNavButton("Base 64", () -> {
             MainWindow.addNavigateItem(MainWindowRoutes.BASE64_ROUTE);
         });
-        box.add(base64Btn);
-
-        SideNavButton hashBtn = new SideNavButton("Hash",()->{
+        SideNavButton hashBtn = new SideNavButton("Hash", () -> {
             MainWindow.addNavigateItem(MainWindowRoutes.HASH_ROUTE);
         });
-        box.add(hashBtn);
-
-        SideNavButton jwtBtn = new SideNavButton("JWT",()->{
+        SideNavButton jwtBtn = new SideNavButton("JWT", () -> {
             MainWindow.addNavigateItem(MainWindowRoutes.JWT_ROUTE);
         });
-        box.add(jwtBtn);
 
-        box.setAlignmentY(Box.CENTER_ALIGNMENT);
+        // Set the layout to GridLayout to ensure equal width
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 0, 10)); // 3 rows, 1 column, with a vertical gap of 10 pixels
+        buttonPanel.setOpaque(false); // Make the panel transparent to match the background
 
+        buttonPanel.add(base64Btn);
+        buttonPanel.add(hashBtn);
+        buttonPanel.add(jwtBtn);
+
+        box.add(buttonPanel);
+
+        box.setAlignmentX(Box.CENTER_ALIGNMENT);
     }
 }
